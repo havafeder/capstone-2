d55e4f85c09989f52ba4f3ac31720d05a335d2ee
Hava Feder <94397782+havafeder@users.noreply.github.com>
2023-01-16 17:03:06 -0800

    Update and rename EditTodo.test.js to TodoEdit.test.js

--- src/components/dashboard/todolist/TodoEdit.test.js
+++ src/components/dashboard/todolist/TodoEdit.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import EditTodo from "./EditTodo";
+import TodoEdit from "./EditTodo";
 import { MemoryRouter } from "react-router";
 
 it("matches snapshot", function () {
 	const { asFragment } = render(
 		<MemoryRouter>
-			<EditTodo todo='Be kind'/>
+			<TodoEdit todo='Be kind'/>
 		</MemoryRouter>,
 	);
 	expect(asFragment()).toMatchSnapshot();
