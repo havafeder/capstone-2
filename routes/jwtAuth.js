bc5f4e8a7fb9f1d0be94c51fc802333a20219425
Hava Feder <94397782+havafeder@users.noreply.github.com>
2023-01-18 21:45:31 -0800

    Update jwtAuth.js

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -15,7 +15,7 @@ router.post('/register', validInfo, async (req, res) => {
 
 		//check if user exists (if yes throw error)
 
-		const user = await pool.query('SELECT * FROM users WHERE user_email = $1',
+		const user = await pool.query('SELECT user_email FROM users WHERE user_email = $1',
 		[email]);
 
 		if (user.rows.length !== 0) {
