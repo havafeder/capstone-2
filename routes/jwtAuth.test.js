a4713ff426c2896058d4e8e3039e5ced4277cc86
Hava Feder <94397782+havafeder@users.noreply.github.com>
2023-01-20 11:30:28 -0800

    Update jwtAuth.test.js

--- routes/jwtAuth.test.js
+++ routes/jwtAuth.test.js
@@ -15,18 +15,18 @@ describe('POST /register', () => {
 		
 		it('should respond with a 200 status code', async () => {
 			const res = await request(app).post('/auth/register').send({
-				name: 'uuuu',
-				email: 'uuuuu@gmail.com',
-				password: 'u6-password'
+				name: 'test',
+				email: 'test@gmail.com',
+				password: 'test-password'
 			});
 			expect(res.statusCode).toBe(200);
 		}, 15000);
 
 		it('should respond with json', async () => {
 			const res = await request(app).post('/auth/register').send({
-				name: 'uuuuuuuuu',
-				email: 'uuuuuuuu@gmail.com',
-				password: 'u7-password'
+				name: 'test',
+				email: 'test@gmail.com',
+				password: 'test-password'
 			});
 			expect(res.headers['content-type']).toEqual(expect.stringContaining('json'));
 		}, 15000);
